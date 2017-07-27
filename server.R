767e8e7a0d347028808fb1825dfedbb46c8172df
Saatvik Ramisetty <SRamisetty@Athena-Connotate.local>
2017-07-27 18:31:35 -0400

    Initial

---
+++ server.R
@@ -0,0 +1,38 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(data.table)
+
+dataset_art <- read_csv("~/shiny_saatvik/data/data_phillips.csv")
+
+
+shinyServer(function(input, output) {
+     
+     output$CompPlot <- renderPlot({
+     
+     df = dataset_art %>% select(artist,price_usd_hammer,yearCreated,result) %>% filter(result == "sold")  %>% filter(artist == input$artist1)   
+     df$yearCreated = as.numeric(df$yearCreated) 
+     
+     df = dataset_art %>% select(artist,price_usd_hammer,yearCreated,result) %>% filter(result == "sold")  %>% filter(artist == input$artist1) %>% group_by(artist,yearCreated) %>% summarise(total = n())
+     df = subset(df,yearCreated >= 1900 & yearCreated <= 2017)
+          
+     ggplot(data=df, 
+                aes(x=as.numeric(yearCreated), y=total, group=artist, colour=artist)) +
+                geom_line() +
+                geom_point()
+     })
+     
+     # output$table1 <-  renderDataTable({
+     #           dataset_art <- dataset_art[dataset_art$artist == input$artist1,]
+     # 
+     # })
+
+})
---
+++ ui.R
@@ -0,0 +1,31 @@
+
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+
+library(shiny)
+library(markdown)
+
+
+shinyUI
+(
+          fluidPage
+          (
+               titlePanel("Athena - Test"),
+     
+               sidebarLayout
+               (
+                    sidebarPanel(selectInput("artist1", "Enter Artist", choices = c("ANDY WARHOL","PABLO PICASSO","CHRISTO") )),
+               
+                    mainPanel
+                    (
+                         plotOutput("CompPlot"),
+                    # DT::dataTableOutput("table1")
+                    )
+                )
+          )
+)
